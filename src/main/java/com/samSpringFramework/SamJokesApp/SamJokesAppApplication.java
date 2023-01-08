d11365d34ee216f93884eee02df7b040be7f9caf
nellaisamaravind <nellaisamaravind@gmail.com>
2023-01-08 07:00:24 +0530

    The Project Created

---
+++ src/main/java/com/samSpringFramework/SamJokesApp/SamJokesAppApplication.java
@@ -0,0 +1,13 @@
+package com.samSpringFramework.SamJokesApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamJokesAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SamJokesAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/samSpringFramework/SamJokesApp/SamJokesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samSpringFramework.SamJokesApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamJokesAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
